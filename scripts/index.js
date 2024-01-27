39da6668b15dd31fc8642705ac1b4c7262ca9d3e
Juriy Rezepin <finskishpion@ya.ru>
2024-01-27 16:17:44 +0300

    финальный 2

--- scripts/index.js
+++ scripts/index.js
@@ -9,20 +9,23 @@
 // @todo: Вывести карточки на страницу
 
 const cardSection = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 function createCard (element, deleteCard) {
-  const cardElement = document.querySelector('#card-template').content.cloneNode(true);
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   cardElement.querySelector('.card__title').textContent = element.name;
   cardImage.alt = element.name;
   cardImage.src = element.link;
   
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', deleteCard);
+  const buttonDelete = cardElement.querySelector('.card__delete-button');
+  buttonDelete.addEventListener("click", () => deleteCard(cardElement));
 
   return cardElement;
 }
 
-const deleteCard = (evt) => evt.target.parentElement.remove();
+function deleteCard(cardElement) {
+  cardElement.remove()
+}
 
 initialCards.forEach(element => cardSection.append(createCard(element, deleteCard)));
