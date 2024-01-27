6aff3274e3005951e99043ea947d1493a26a1eae
Juriy Rezepin <finskishpion@ya.ru>
2024-01-27 12:42:56 +0300

    финальный

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/finskishpion/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,22 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardSection = document.querySelector('.places__list');
+
+function createCard (element, deleteCard) {
+  const cardElement = document.querySelector('#card-template').content.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardImage.alt = element.name;
+  cardImage.src = element.link;
+  
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
+}
+
+const deleteCard = (evt) => evt.target.parentElement.remove();
+
+initialCards.forEach(element => cardSection.append(createCard(element, deleteCard)));
